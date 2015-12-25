b5e192f226a14b0ba97b5c99af0415b1429e3e87
ymgn <gonyamagon@gmail.com>
2015-12-25 14:08:04 +0900

    いらない部分のCSS削除　margintopなど　　ただしログインできないため見れてはいない dbはエラーがなんか名前に付け加えたら治るって書いててやったら治った痕跡

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_prod',
+			'dsn'        => 'mysql:host=localhost;dbname=fuel_prod',//fuel_prod
 			'username'   => 'fuel_app',
 			'password'   => 'super_secret_password',
 		),
