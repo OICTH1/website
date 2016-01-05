d795b8adbadb9c4258bb398e86e60ff460743a23
44x1carbon <b4079@oic.jp>
2016-01-04 19:45:59 -0800

    商品リスト 商品数が３の倍数以外でもレイアウトが崩れないように

--- fuel/app/views/content/itemlist.php
+++ fuel/app/views/content/itemlist.php
@@ -11,26 +11,36 @@
 		</ul>
 	</div>
 	<div class="list">
-			<?php foreach ( $item_list as $item) :?>
-            <div class="listitem">
-                <div class="img">
-                  <?php echo Asset::img($item['img_path']);?>
-                </div>
-                <div class="pizzaname"><?php echo $item['item_name']?></div>
-				<div class="pricelist">
-					<?php if($item['prices']['unit_price'] != null):?>
-					 	<div class="price">￥<span id="price"><?php echo $item['prices']['unit_price']?></span></div>
-					<?php else:?>
-				   		<div class="price-sml">S￥<span id="price_s"><?php echo $item['prices']['unit_price_s']?></span> M￥<span id="price_m"><?php echo $item['prices']['unit_price_m']?></span> L￥<span id="price_l"><?php echo $item['prices']['unit_price_l']?></span></div>
-			   		<?php endif;?>
-				</div>
-	            <div class="zei">(税込)</div>
-                <div class="description"><?php echo $item['explanatory']?></div>
-                <div class="syousai">
-					<?php echo Html::anchor('index.php/item/detail/' . $item['item_id'],'詳細を見る',array('class'=>'buttonlink'))?>
-                </div>
-            </div>
-		<?php endforeach; ?>
+			<?php $i = 0;foreach ( $item_list as $item) :?>
+			<?php if($i % 3 == 0){echo '<div class="listrow">';}?>
+	            <div class="listitem">
+	                <div class="img">
+	                  <?php echo Asset::img($item['img_path']);?>
+	                </div>
+	                <div class="pizzaname"><?php echo $item['item_name']?></div>
+					<div class="pricelist">
+						<?php if($item['prices']['unit_price'] != null):?>
+						 	<div class="price">￥<span id="price"><?php echo $item['prices']['unit_price']?></span></div>
+						<?php else:?>
+					   		<div class="price-sml">S￥<span id="price_s"><?php echo $item['prices']['unit_price_s']?></span> M￥<span id="price_m"><?php echo $item['prices']['unit_price_m']?></span> L￥<span id="price_l"><?php echo $item['prices']['unit_price_l']?></span></div>
+				   		<?php endif;?>
+					</div>
+		            <div class="zei">(税込)</div>
+	                <div class="description"><?php echo $item['explanatory']?></div>
+	                <div class="syousai">
+						<?php echo Html::anchor('index.php/item/detail/' . $item['item_id'],'詳細を見る',array('class'=>'buttonlink'))?>
+	                </div>
+	            </div>
+			<?php if($i % 3 == 2){echo '</div>';}?>
+		<?php $i++;endforeach; ?>
+		<?php $j = $i % 3;
+			if($j != 0 ){
+				for ($k=0; $k < 3-$j; $k++) {
+					echo '<div class="listitem noneitem"></div>';
+				}
+				echo '</div>';
+			}
+		?>
 		<div style="clear:both"></div>
     </div>
 	<div class="toppage">
