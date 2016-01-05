6815149949075ef7ee3bcd253937ba2d9045f7dc
44x1carbon <b4079@oic.jp>
2016-01-04 22:57:36 -0800

    商品詳細デザイン変更

--- fuel/app/views/content/itemdetail.php
+++ fuel/app/views/content/itemdetail.php
@@ -5,39 +5,39 @@
     商品詳細
   </div>
   <div class="menuin">
-          <div class="menuname"><?php echo $detail['item_name']?></div>
-    <div class="menuleft">
-      <?php echo Asset::img($detail['img_path']); ?>
+    <div class="detail">
+        <div class="menuname"><?php echo $detail['item_name']?></div>
+        <div class="menuleft">
+          <?php echo Asset::img($detail['img_path']); ?>
+        </div>
+        <div class="menuright">
+                  <!--<div class="pricelist">-->
+                     <?php if($detail['prices']['unit_price'] != null ):?>
+                         ￥<span id="price"><?php echo $detail['prices']['unit_price']?></span>
+                     <?php else:?>
+                        S￥<span id="price_s"><?php echo $detail['prices']['unit_price_s']?></span> M￥<span id="price_m"><?php echo $detail['prices']['unit_price_m']?></span> L￥<span id="price_l"><?php echo $detail['prices']['unit_price_l']?></span>
+                    <?php endif;?>
+                  <!--</div>-->
+                  <!--<div class="zei">-->(税込)<br><!--</div>-->
+          <!--<div class="description">--><?php echo $detail['explanatory']?><!--</div>-->
+        </div>
+        <div class="clear"></div>
+        <?php if(isset($order_id)):?>
+            <?php echo Form::open(array('action' => 'index.php/cart/edit/' . $order_id, 'method' => 'post'))?>
+        <?php else :?>
+            <?php echo Form::open(array('action' => 'index.php/cart/add', 'method' => 'post'))?>
+        <?php endif;?>
+        <?php echo Form::hidden('item_id',$detail['item_id'])?>
+        <?php if(!($detail['prices']['unit_price'] != null)):?>
     </div>
-    <div class="menuright">
-              <div class="pricelist">
-                 <?php if($detail['prices']['unit_price'] != null ):?>
-                      <div class="price">￥<span id="price"><?php echo $detail['prices']['unit_price']?></span></div>
-                 <?php else:?>
-                    <div class="price-sml">S￥<span id="price_s"><?php echo $detail['prices']['unit_price_s']?></span> M￥<span id="price_m"><?php echo $detail['prices']['unit_price_m']?></span> L￥<span id="price_l"><?php echo $detail['prices']['unit_price_l']?></span></div>
-                <?php endif;?>
-              </div>
-              <div class="zei">(税込)</div>
-      <div class="description"><?php echo $detail['explanatory']?></div>
-    </div>
-    <?php if(isset($order_id)):?>
-        <?php echo Form::open(array('action' => 'index.php/cart/edit/' . $order_id, 'method' => 'post'))?>
-    <?php else :?>
-        <?php echo Form::open(array('action' => 'index.php/cart/add', 'method' => 'post'))?>
-    <?php endif;?>
-    <?php echo Form::hidden('item_id',$detail['item_id'])?>
-    <?php if(!($detail['prices']['unit_price'] != null)):?>
     <div class="size">
       <div class="sizeheading">
         サイズ
       </div>
       <div class="sizein">
-              <label><div class="s">					Sサイズ<br><input type="radio" name="size" value="_s" checked>
-        </div></label>
-              <label><div class="m">					Mサイズ<br><input type="radio" name="size" value="_m">
-        </div></label>
-              <label><div class="l">					Lサイズ<br><input type="radio" name="size" value="_l">
-        </div></label>
+              <div class="s"><label>Sサイズ<br><input type="radio" name="size" value="_s" checked></label></div>
+              <div class="m"><label>Mサイズ<br><input type="radio" name="size" value="_m"></label></div>
+              <div class="l"><label>Lサイズ<br><input type="radio" name="size" value="_l"></label></div>
       </div>
     </div>
     <?php endif;?>
@@ -59,13 +59,14 @@
         <?php if(isset($order_id)):?>
             <input type="submit" value="変更" >
         <?php else :?>
-            <input type="submit" value="カートへ入れる" >
+            <input type="submit" value="カートへ入れる">
         <?php endif;?>
       </div>
       <?php echo Form::close()?>
+    </div>
+  </div>
 </div>
-</div>
-<div class="list">
-  <?php echo Html::anchor('index.php/item/list/'.$detail['category'],'一覧へ戻る',array('class'=>'buttonlink'))?>
+<div class="button">
+  <?php echo Html::anchor('index.php/item/list/'.$detail['category'],'一覧へ戻る',array('class'=>'link'))?>
 </div>
 <?php echo Asset::js('content/itemdetail.js')?>
