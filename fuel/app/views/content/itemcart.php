fe3a97ee3294a385e31ea114035597ed43cc67ad
44x1carbon <b4079@oic.jp>
2016-01-05 21:43:11 -0800

    カートデザイン変更

--- fuel/app/views/content/itemcart.php
+++ fuel/app/views/content/itemcart.php
@@ -7,35 +7,44 @@
             ご注文内容
         </div>
         <div class="cart">
-            <?php if(isset($cart['orders'])):?>
-                <?php foreach ($cart['orders'] as $key => $order) :?>
-                    <div class="cartin">
-                        <div class="name">
-                            <span><?php echo $order['item_name']?></span><span><?php if ($order['size']) {echo '(' . $order['size'] .')';};?></span>
+            <div class="orders">
+                <?php if(isset($cart['orders'])):?>
+                    <?php foreach ($cart['orders'] as $key => $order) :?>
+                        <div class="order">
+                            <div class="name">
+                                <span><?php echo $order['item_name']?></span><span><?php if ($order['size']) {echo '(' . $order['size'] .')';};?></span>
+                            </div>
+                            <div class="order-bottom">
+                                <div>
+                                    <div class="num">
+                                        数量<span><?php echo $order['quantity']?></span>
+                                    </div>
+                                    <div class="money">
+                                        金額￥<span><?php echo $order['money']?></span>
+                                    </div>
+                                    <div class="change button">
+                                        <?php echo Html::anchor('index.php/item/detail/'.$order['item_id'] . '/' . $key,'変更',array('class'=>'link'))?>
+                                    </div>
+                                    <div class="delete button">
+                                        <?php echo Html::anchor('index.php/cart/delete/' . $key,'削除',array('class'=>'link'))?>
+                                    </div>
+                                    <div class="clear"></div>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="clear"></div>
                         </div>
-                        <div class="num">
-                            数量<span><?php echo $order['quantity']?></span>
-                        </div>
-                        <div class="money">
-                            金額￥<span><?php echo $order['money']?></span>
-                        </div>
-                        <div class="change">
-                            <?php echo Html::anchor('index.php/item/detail/'.$order['item_id'] . '/' . $key,'変更',array('class'=>'cartinbutton'))?>
-                        </div>
-                        <div class="delete">
-                            <?php echo Html::anchor('index.php/cart/delete/' . $key,'削除',array('class'=>'cartinbutton'))?>
-                        </div>
-                    </div>
-                <?php endforeach;?>
-            <?php endif;?>
-          </div>
-        <div class="summoney">
-            ご注文金額　<span><?php echo $cart['total_money']?></span>円
+                    <?php endforeach;?>
+                <?php endif;?>
+              </div>
+            <div class="summoney">
+                ご注文金額　<span><?php echo $cart['total_money']?></span>円
+            </div>
         </div>
-        <div class="deliverybutton">
-            <?php echo Html::anchor('index.php/order/delivery','配達先選択へ進む',array('class'=>'buttonlink'))?>
+        <div class="button selectdelivery">
+            <?php echo Html::anchor('index.php/order/delivery','配達先選択へ進む',array('class'=>'link'))?>
         </div>
-        <div class="itemlistbutton">
-          <?php echo Html::anchor('index.php/item/list','お買い物を続ける',array('class'=>'buttonlink'))?>
+        <div class="button">
+          <?php echo Html::anchor('index.php/item/list','お買い物を続ける',array('class'=>'link'))?>
         </div>
       </div>
