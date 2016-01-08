50302d5bd07b8c36c35d6c1904a9bc7308733830
44x1carbon <b4079@oic.jp>
2016-01-07 18:12:34 -0800

    会員登録完了メッセージ　内容変更

--- fuel/app/views/message/newmember.php
+++ fuel/app/views/message/newmember.php
@@ -1,8 +1,3 @@
 
-<p>注文が完了いたしました<br/>
-商品が届くまでお待ちくださいませ</p>
-
-
-<div class="top button">
-    <?php echo Html::anchor('index.php/top','トップページへ戻る',array('class'=>'buttonlink'))?>
-</div>
+<p>ご登録ありがとうございます<br/>
+お買い物をお楽しみください</p>
